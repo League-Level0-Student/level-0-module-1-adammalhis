82aee73831eaca7054027d806c4b0ea8f3d7a1a0
League Student <league@iMac-8.attlocal.net>
2018-09-22 17:28:51 -0700

    RemarkableIP

---
+++ src/ifs_ints_and_loops/Remarable.java
@@ -0,0 +1,15 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Remarable {
+public static void main(String[] args) {
+
+	String mike = "you are awsome";
+	String Bob = "you are so cool";
+	String Christaphor = "you are good";
+	String Brandon = "you are bossy";
+	JOptionPane.showInputDialog(null,"what is your name");
+
+}
+}
--- src/ifs_ints_and_loops/XGeeks.java
+++ src/ifs_ints_and_loops/XGeeks.java
@@ -15,12 +15,14 @@ import javax.swing.JOptionPane;
 public class XGeeks {
 public static void main(String[] args) {
 
+	
+	
 		// 1. Save the superpower for each person in a variable.
-
-		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String mike="nothing";
+	// 2. Ask the user to enter a name. Store their answer in a variable.
 
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+	
 	}
 }
 
