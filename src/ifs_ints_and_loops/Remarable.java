fffc380c1aed415c84cf40e3ee0ccde306da0ad3
League Student <league@iMac-8.attlocal.net>
2018-09-29 17:33:35 -0700

    Remarkable, Seceret Message Box

--- src/ifs_ints_and_loops/Remarable.java
+++ src/ifs_ints_and_loops/Remarable.java
@@ -6,10 +6,24 @@ public class Remarable {
 public static void main(String[] args) {
 
 	String mike = "you are awsome";
-	String Bob = "you are so cool";
+	String Bob = "Hello Idiot";
 	String Christaphor = "you are good";
 	String Brandon = "you are bossy";
-	JOptionPane.showInputDialog(null,"what is your name");
-
+	String TheirName=JOptionPane.showInputDialog(null,"what is your name");
+	
+	if (TheirName.equals("Bob")){
+		JOptionPane.showMessageDialog(null, Bob);
+	}	
+	if (TheirName.equals("mike")){
+		JOptionPane.showMessageDialog(null, mike);
+	}
+	if (TheirName.equals("Christaphor")){
+		JOptionPane.showMessageDialog(null, Christaphor);
+	}
+		if (TheirName.equals("Brandon")){
+			JOptionPane.showMessageDialog(null, Brandon);
+		}
+	
 }
 }
+	
--- src/ifs_ints_and_loops/Seceretmessagebox.java
+++ src/ifs_ints_and_loops/Seceretmessagebox.java
@@ -8,11 +8,13 @@ public static void main(String[] args) {
 	
 	
 	String password="randomnumbers";
-	JOptionPane.showInputDialog(null,"type in a seceret message");
-	JOptionPane.showInputDialog(null,"you can see the seceret message if you guess the password");
-	if()
-	
-	
-	
+	String TheMessage=JOptionPane.showInputDialog(null,"type in a seceret message");
+	String hi = JOptionPane.showInputDialog(null,"you can see the seceret message if you guess the password");
+	if(hi.equals(password)) {
+	JOptionPane.showMessageDialog(null,TheMessage);}
+	else
+	JOptionPane.showMessageDialog(null,"INTRUTER!!!!!!!!!!!");
+
 }
 }
+
