2687e3c743970010d85a64d9f8ac0e8254f41ec6
league <league@wts22.attlocal.net>
2018-10-20 17:15:03 -0700

    My Ages and Badgers

---
+++ src/ifs_ints_and_loops/Badgers.java
@@ -0,0 +1,22 @@
+package ifs_ints_and_loops;
+
+public class Badgers {
+	public static void main(String[] args) {
+
+		for (int b = 0; b < 3; b++) {
+
+			for (int i = 0; i < 12; i++) {
+				System.out.println("Badgers");
+			}
+
+			for (int k = 0; k < 3; k++) {
+				System.out.println("Mushroom");
+			}
+
+		}
+		for (int s = 0; s < 3; s++) {
+			System.out.println("snake");
+		}
+
+	}
+}
